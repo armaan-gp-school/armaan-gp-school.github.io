de45c6beb05b5ffaff0ec0ddf76cacca98840793
armaan-gp-school <hcps-guptaa5@henricostudents.org>
2024-11-19 15:48:58 +0000

    upd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var home_game_score = "Love";
+var home_game_score = "0";
 var home_set_score = 0;
 var home_match_score = 0;
-var visitor_game_score = "Love";
+var visitor_game_score = "0";
 var visitor_set_score = 0;
 var visitor_match_score = 0;
 
